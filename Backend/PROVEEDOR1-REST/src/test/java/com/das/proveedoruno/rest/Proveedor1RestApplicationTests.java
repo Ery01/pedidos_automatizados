0add45cd06cf211c21306dadc54b0937405bb3b9
Gino Ventura <ginoventura12@gmail.com>
2025-01-08 22:17:24 -0300

    Se agregan proyectos base

---
+++ Backend/PROVEEDOR1-REST/src/test/java/com/das/proveedoruno/rest/Proveedor1RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.das.proveedoruno.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proveedor1RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
